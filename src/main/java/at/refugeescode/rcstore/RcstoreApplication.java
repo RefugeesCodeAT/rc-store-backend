648f3a7a9184b2c5f7dacf70ed77cd8c75499cab
nana80802 <miss_nisreen2000@yahoo.com>
2018-05-04 12:05:04 +0200

    Add spring structure

---
+++ src/main/java/at/refugeescode/rcstore/RcstoreApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.rcstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcstoreApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/rcstore/RcstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.rcstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RcstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
