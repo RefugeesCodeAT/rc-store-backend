f8fd474812f8a3eb61730621aeb2dcab66d844e6
Fozat Kardouh <fozatkardouh@gmail.com>
2018-05-17 18:51:25 +0200

    added connection to database and annded junit5 testing dependency

--- src/test/java/at/refugeescode/rcstore/RcstoreApplicationTests.java
+++ src/test/java/at/refugeescode/rcstore/RcstoreApplicationTests.java
@@ -1,16 +1,16 @@
 package at.refugeescode.rcstore;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class RcstoreApplicationTests {
+@ExtendWith(SpringExtension.class)
+class RcstoreApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
