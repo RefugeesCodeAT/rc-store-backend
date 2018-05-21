730d2b4347f97acee978b6eeb071e7f85d9b7956
yazankassoua <yazan.kassoua@gmail.com>
2018-05-21 16:15:02 +0200

    finished register page

--- src/main/java/at/refugeescode/rcstore/controller/logic/RegisterService.java
+++ src/main/java/at/refugeescode/rcstore/controller/logic/RegisterService.java
@@ -1,46 +1,9 @@
 package at.refugeescode.rcstore.controller.logic;
 
-import at.refugeescode.rcstore.models.User;
-import at.refugeescode.rcstore.persistence.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+import at.refugeescode.rcstore.models.UserDto;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+public interface RegisterService {
 
-@Service
-@RequiredArgsConstructor
-public class RegisterService {
-
-    private final PasswordEncoder passwordEncoder;
-    private final UserRepository userRepository;
-
-    public String controlUser(User newUser){
-        Optional<User> optionalUser = userRepository.findOneByEmail(newUser.getEmail());
-        if (optionalUser.isPresent()) {
-            return "redirect:/register";
-        }
-        register(newUser);
-        return "redirect:/login";
-    }
-
-    private void register(User newUser) {
-        encodePassword(newUser);
-        setRolesToUser(newUser);
-        userRepository.save(newUser);
-    }
-
-    private void encodePassword(User newUser) {
-        String encodedPassword = passwordEncoder.encode(newUser.getPassword());
-        newUser.setPassword(encodedPassword);
-    }
-
-    private void setRolesToUser(User newUser) {
-        Set<String> userRoles = Stream.of("ROLE_USER").collect(Collectors.toSet());
-        newUser.setRoles(userRoles);
-    }
+    String controlUser(UserDto newUser);
 
 }
---
+++ src/main/java/at/refugeescode/rcstore/controller/logic/RegisterServiceImp.java
@@ -0,0 +1,60 @@
+package at.refugeescode.rcstore.controller.logic;
+
+import at.refugeescode.rcstore.models.User;
+import at.refugeescode.rcstore.models.UserDto;
+import at.refugeescode.rcstore.persistence.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+@RequiredArgsConstructor
+public class RegisterServiceImp implements RegisterService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    @Override
+    public String controlUser(UserDto userDto) {
+        Optional<User> optionalUser = userRepository.findOneByEmail(userDto.getEmail());
+        if (optionalUser.isPresent()) {
+            return "redirect:/register";
+        }
+        if (!userDto.getPassword().equals(userDto.getMatchingPassword())){
+            return "redirect:/register";
+        }
+        register(userDto);
+        return "redirect:/login";
+    }
+
+    private void register(UserDto userDto) {
+        createNewUserFrom(userDto);
+    }
+
+    private void createNewUserFrom(UserDto userDto) {
+        User newUser = new User();
+        newUser.setFirstName(userDto.getFirstName());
+        newUser.setLastName(userDto.getLastName());
+        newUser.setEmail(userDto.getEmail());
+        newUser.setPassword(userDto.getPassword());
+        encodePassword(newUser);
+        setRolesToUser(newUser);
+        userRepository.save(newUser);
+    }
+
+    private void encodePassword(User newUser) {
+        String encodedPassword = passwordEncoder.encode(newUser.getPassword());
+        newUser.setPassword(encodedPassword);
+    }
+
+    private void setRolesToUser(User newUser) {
+        Set<String> userRoles = Stream.of("ROLE_USER").collect(Collectors.toSet());
+        newUser.setRoles(userRoles);
+    }
+
+}
